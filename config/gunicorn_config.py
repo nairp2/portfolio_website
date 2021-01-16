1bec439ff1c5a2b79b536206c1572639da6da705
nairp2 <nairp2@tcnj.edu>
2021-01-16 00:55:25 -0500

    First commit

---
+++ config/gunicorn_config.py
@@ -0,0 +1,4 @@
+command = '/Users/pranavnair/Desktop/portfolio_website/portfolio_website/env/bin/gunicorn'
+pythonpath = '/Users/pranavnair/Desktop/portfolio_website/portfolio_website'
+bind = '167.99.192.225:8000'
+workers = 3
